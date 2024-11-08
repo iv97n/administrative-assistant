22474f2aa84726d51efa87b498705ed57366e534
iv97n <ivan.heg.03@gmail.com>
2024-11-09 00:34:56 +0100

    feat(scraper): improve html parsing

--- src/scraper/scraper/spiders/upf_spider.py
+++ src/scraper/scraper/spiders/upf_spider.py
@@ -1,10 +1,11 @@
 import scrapy
 import os
+import re
 from urllib.parse import urlparse
 
 class UpfSpider(scrapy.Spider):
     name = 'upf_spider'
-    start_urls = ['https://www.upf.edu/']
+    start_urls = ['https://www.upf.edu/web/secretaria-grau/acreditacio-de-coneixements-d-idiomes']
 
     custom_settings = {
         'DEFAULT_REQUEST_HEADERS': {
@@ -14,15 +15,43 @@ class UpfSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-         # Create a unique file name based on the URL or any other identifier
         filename = f"output_{response.url.split('/')[-1]}.data"
 
-        with open(filename, 'wb') as f:
-                f.write(response.body)
+        content = response.body.decode('utf-8')  
+    
+        content = self.clean_html(content)
+        
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(content)
                 
-        # Follow all internal links on the page (excluding external links)
         for link in response.css('a::attr(href)').getall():
-            # Make sure the link is a valid internal URL
             if link and link.startswith('/') or link.startswith(response.url):
                 absolute_url = response.urljoin(link)
                 yield scrapy.Request(absolute_url, callback=self.parse)
+    
+    
+    def clean_html(self, text):
+        # 1. Remove <script> and <style> content
+        text = re.sub(r'<script.*?>.*?</script>', '', text, flags=re.DOTALL)
+        text = re.sub(r'<style.*?>.*?</style>', '', text, flags=re.DOTALL)
+
+        # 2. Remove HTML comments
+        text = re.sub(r'<!--.*?-->', '', text, flags=re.DOTALL)
+
+        # 3. Extract relevant content from <h1>, <h2>, <h3>, <p>, <ul>, <ol>, <li>, <a>
+        text = re.sub(r'<h1.*?>(.*?)</h1>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<h2.*?>(.*?)</h2>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<h3.*?>(.*?)</h3>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<p.*?>(.*?)</p>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<a.*?>(.*?)</a>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<ul.*?>(.*?)</ul>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<ol.*?>(.*?)</ol>', r'\1\n', text, flags=re.DOTALL)
+        text = re.sub(r'<li.*?>(.*?)</li>', r'\1\n', text, flags=re.DOTALL)
+
+        # 4. Clean up any remaining HTML tags
+        text = re.sub(r'<.*?>', '', text)
+
+        text = '\n'.join([line.strip() for line in text.splitlines()])
+
+        return '\n'.join([line for line in text.splitlines() if line.strip() != ''])
+    
