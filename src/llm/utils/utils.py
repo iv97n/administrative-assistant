c44b767fbe8fcd3c3c706608523961a96a8a203d
BrunoManzano <bruno.manzano01@estudiant.upf.edu>
2024-11-09 10:03:39 +0100

    parse document

--- src/llm/utils/utils.py
+++ src/llm/utils/utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import base64
 
 
 def retrieve_documents(mongo_uri, mongo_db, mongo_collection):
@@ -8,3 +9,12 @@ def retrieve_documents(mongo_uri, mongo_db, mongo_collection):
         
         for doc in collection.find():
                 yield doc
+
+
+def parse_document(doc):
+    # Decode the base64 content
+    binary_content = base64.b64decode(doc["content"]["$binary"]["base64"])
+    # Convert to a string (assuming UTF-8 encoding) and return it
+    text_content = binary_content.decode("utf-8")
+    
+    return text_content
